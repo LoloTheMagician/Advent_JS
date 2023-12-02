a3dd7a06986a569bcc63459e8b9be80efb89d818
Lolo <elperiplodelolo@gmail.com>
2023-12-02 16:02:11 +0100

    Primera parte del reto 1 cumplida!

--- advent_of_code/2023/challenge_1/challenge_1_a.py
+++ advent_of_code/2023/challenge_1/challenge_1_a.py
@@ -1,11 +1,20 @@
-import read_file
+def read_file(input):
+    file = open(input, "r")
+    file_read = file.read()
+    file_split = file_read.splitlines()
+    return file_split
 
-file = read_file.read_file("input.txt")
+
+file = read_file("input.txt")
 
 
 def print_line(lines):
+    all_numbs = []
     for line in lines:
-        print(line)
+        just_digits = list(filter(lambda symbol: symbol.isdigit(), list(line)))
+        first, last = just_digits[0], just_digits[len(just_digits) - 1]
+        all_numbs.append(int(first + last))
+    return sum(all_numbs)
 
 
-print_line(file)
+print(print_line(file))
