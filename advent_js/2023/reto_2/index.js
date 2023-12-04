b9c9e3210d4fc382b308bbfe01ee26e718c3f766
Lolo <elperiplodelolo@gmail.com>
2023-12-04 09:39:24 +0100

    Retos 2 y 3 nuevas versiones.

--- advent_js/2023/reto_2/index.js
+++ advent_js/2023/reto_2/index.js
@@ -13,18 +13,47 @@
 //     return gifts
 //         .filter(gift => no_letters.every(letter => !gift.includes(letter)))
 // }
-/**
- *
- *
- * @param {*} regalos
- * @param {string} materiales
- */
-const manufacture = (regalos, materiales) => {
-    const string = [...new Set(regalos.join(''))]
-    const no_l = string
-        .filter(l => !materiales.includes(l))
-    return regalos
-        .filter(regalo => !no_l.every(l => !regalo.includes(l)))
+
+// /**
+//  *
+//  *
+//  * @param {*} regalos
+//  * @param {string} materiales
+//  */
+// const manufacture = (regalos, materiales) => {
+//     const string = [...new Set(regalos.join(''))]
+//     const no_l = string
+//         .filter(l => !materiales.includes(l))
+//     return regalos
+//         .filter(regalo => !no_l.every(l => !regalo.includes(l)))
+// }
+
+const manufacture = (gifts, materials) => {
+    let gifts_no_materials = [...gifts]
+    materials
+        .split('')
+        .forEach(letter => {
+            gifts_no_materials = gifts_no_materials.map(gift =>
+                gift.replaceAll(letter, '')
+            )
+        })
+    return gifts.filter((_, i) => gifts_no_materials[i] === '')
+}
+
+function manufacture(gifts, materials) {
+    const contadores = [0, 0, 0]
+    materials
+        .split('')
+        .forEach(letter => {
+            gifts.forEach((gift, index) => {
+                if (gift.includes(letter))
+                    contadores[index]++
+            })
+        })
+    return gifts
+        .filter((gift, index) =>
+            new Set(gift).size === contadores[index]
+        )
 }
 
 console.log(
